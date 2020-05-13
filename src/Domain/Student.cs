4d2fe92c017770c28342a83adf41bcb03c843c94
Alexsander Muniz <alexsandermunizs@gmail.com>
2020-05-12 22:20:05 -0300

    Criacao das classes de dominio

---
+++ src/Domain/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Domain
+{
+    public class Student : IStudent
+    {
+        /// <summary>
+        ///     Nome do Aluno.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     Idade do Aluno.
+        /// </summary>
+        public int Age { get; set; }
+
+        /// <summary>
+        ///     Matérias com a matrícula efetivada.
+        /// </summary>
+        public List<ISubject> RegistredSubjects { get; set; }
+
+        /// <summary>
+        ///     Matérias com interesse de matricular.
+        /// </summary>
+        public List<ISubject> SelectedSubjects { get; set; }
+
+    }
+}
