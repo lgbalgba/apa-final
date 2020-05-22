bc1cf1d4f02226311c26fa356bceee0b9443da84
Alexsander Muniz <alexsandermunizs@gmail.com>
2020-05-21 22:31:22 -0300

    adicao do servico do estudante

---
+++ src/Domain/IStudentService.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain
+{
+    public interface IStudentService
+    {
+        void StudentSelectDiscipline(Student student, Discipline discipline);
+
+        void StudentRegisterDiscipline(Student student, Discipline discipline);
+
+        void StudentRemoveDisciplineAssignment(Student student, Discipline discipline);
+    }
+}
---
+++ src/Domain/StudentService.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain
+{
+    public class StudentService : IStudentService
+    {
+        public void StudentRegisterDiscipline(Student student, Discipline discipline)
+        {
+            student.RegisterDiscipline(discipline);
+        }
+
+        public void StudentRemoveDisciplineAssignment(Student student, Discipline discipline)
+        {
+            student.RemoveDisciplineAssignment(discipline);
+        }
+
+        public void StudentSelectDiscipline(Student student, Discipline discipline)
+        {
+            student.SelectDiscipline(discipline);
+        }
+    }
+}
